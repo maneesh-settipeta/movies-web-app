ba6f90b5690f5c4701f3506b822b36712c61e1eb
maneesh-settipeta <maneeshsettipeta@gmail.com>
2025-04-29 18:31:23 -0500

    feat: Add new movie images to imagesMap in movieImages.ts

--- src/movieImages.ts
+++ src/movieImages.ts
@@ -6,7 +6,16 @@ imagesMap.set("202", "src/assets/Hey-jawani.jpg");
 imagesMap.set("203", "src/assets/wild-robot.jpg");
 imagesMap.set("204", "src/assets/pushpa1.jpg");
 imagesMap.set("205", "src/assets/mad2.jpg");
-imagesMap.set("000", "/noImage.jpg");
+imagesMap.set("211", "src/assets/musafa.jpg");
+imagesMap.set("213", "src/assets/hit3.jpg");
+imagesMap.set("215", "src/assets/dragon.jpg");
+imagesMap.set("206", "src/assets/Robinhood_2025_Poster.jpg");
+imagesMap.set("207", "src/assets/ja.jpeg");
+imagesMap.set("208", "src/assets/c1.jpg");
+imagesMap.set("209", "src/assets/k1.webp");
+imagesMap.set("210", "src/assets/odela2.jpg");
+imagesMap.set("212", "src/assets/minecraft.jpg");
+
 
 export default imagesMap;
 
